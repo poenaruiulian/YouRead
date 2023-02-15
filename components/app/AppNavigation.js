f640c6f72cde7a91a7114a19023ebcb99b40b0fd
poenaruiulian <poenaruiulian2003@gmail.com>
2023-02-15 19:11:55 +0200

    Started add book manually, need to finish

--- components/app/AppNavigation.js
+++ components/app/AppNavigation.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./Home"
 import { AddBook } from './Home';
 import { BookPage } from './Home';
+import {AddBookManually} from "./Home"
 
 const Stack = createNativeStackNavigator()
 
@@ -13,6 +14,7 @@ export default function AppNavigation(){
             <Stack.Screen options={{headerShown:false,gestureEnabled: false,detachInactiveScreens:true}} name="HomeScreen" component={HomeScreen}/>
             <Stack.Screen options={{headerShown:false,gestureEnabled: false,detachInactiveScreens:true}} name="AddBook" component={AddBook}/>
             <Stack.Screen options={{headerShown:false,gestureEnabled: false,detachInactiveScreens:true}} name="BookPage" component={BookPage}/>
+            <Stack.Screen options={{headerShown:false,detachInactiveScreens:true}} name="AddBookManually" component={AddBookManually}/>
         </Stack.Navigator>
     )
 
